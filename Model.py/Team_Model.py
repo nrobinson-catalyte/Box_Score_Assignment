1c5f0ead9446db7e51f631f7a6a834b7a40a5387
nrobinson-catalyte <nrobinson@catalyte.io>
2026-07-18 08:36:11 -0500

    Team_Model_Completed

---
+++ Model.py/Team_Model.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass
+class Team:
+    Name: str
+    City: str
+    State: str
+    Arena: str
+
+    def __init__(self, Name:str, City: str, State: str, Arena: str):   
+        
+        self.Name = Name
+        self.City = City
+        self.State = State
+        self.Arena = Arena
