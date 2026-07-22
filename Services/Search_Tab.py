3360263d880ea0a4cc432b5ff1355f6eaef7d4e8
nrobinson-catalyte <nrobinson@catalyte.io>
2026-07-22 15:49:42 -0500

    Services Planning (Service_Tab Done)

---
+++ Services/Search_Tab.py
@@ -0,0 +1,16 @@
+# Search Team Stats
+    # I need to be able to find:
+        # The Team      
+        # The Location
+        # The Record of Team
+    # Exceptions
+        # No Team is found.
+# Search Player Stats
+    # I need to be able to find:
+        # Name
+        # College
+        # Points Average
+        # Assist Average
+        # Rebounds Average
+    # Exceptions
+        # No Player is found.
